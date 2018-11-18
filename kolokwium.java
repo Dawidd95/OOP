558d750605d08e43c335b994eeefc978accfb50a
Dawidd95 <32812931+Dawidd95@users.noreply.github.com>
2018-11-18 11:17:26 +0100

    Create kolokwium.java

---
+++ kolokwium.java
@@ -0,0 +1,6 @@
+// dodanie 3 elementow
+// wyswietlenie ich odrazu
+// dodanie elementu sprawdzajac czy juz nie istnieje czasem
+// wyswietlenie wszystkich z menu
+// usuniecie jesli nie ma takiego to mowi o tym
+// modyfikacja wybranego elementu
